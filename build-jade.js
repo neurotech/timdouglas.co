becb745ce93b2f27c7cb21c7198453d1f42eee35
Tim Douglas <neurotech@gmail.com>
2015-05-24 16:48:54 +1000

    Added script by Richardo Pedroni to handle jade building

---
+++ build-jade.js
@@ -0,0 +1,40 @@
+// Original script by Ricardo Pedroni:
+// http://stackoverflow.com/a/28438357/733948
+
+var fs = require('fs');
+var path = require('path');
+var exec = require('child_process').exec;
+
+var jadePath = process.argv[2];
+var base = process.argv[3];
+
+var _build = function(dir) {
+  var dirName, f, files, fullPath, i, len, results, stat;
+  files = fs.readdirSync(dir);
+  results = [];
+  for (i = 0, len = files.length; i < len; i++) {
+    f = files[i];
+    if (f === path.basename(__filename)) {
+      continue;
+    }
+    fullPath = path.join(dir, f);
+    stat = fs.statSync(fullPath);
+    if (stat.isDirectory()) {
+      try {
+        fs.mkdirSync(path.join(base, fullPath));
+      } catch (_error) {}
+      results.push(_build(fullPath));
+    } else if (path.extname(f) === '.jade') {
+      dirName = path.join(base, path.dirname(fullPath));
+      dirName = dirName.replace(jadePath, '');
+      dirName = dirName.replace('//', '/');
+      console.log("Compiling jade file '" + fullPath + "' to directory " + dirName);
+      results.push(exec('jade -o ' + dirName + ' ' + fullPath));
+    } else {
+      results.push(void 0);
+    }
+  }
+  return results;
+};
+
+_build(jadePath);
