35c567897e6d829dc962bfce89cc3ca9d929eafc
Tim Douglas <neurotech@gmail.com>
2015-05-26 21:16:38 +1000

    Added db pushing, dir/file creation, template streaming, support for user input at runtime to new-post script

--- new-post.js
+++ new-post.js
@@ -1,10 +1,43 @@
+var prompt = require('prompt');
 var low = require('lowdb');
-var db = low('db.json');
+var fs = require('fs');
+var moment = require('moment');
 
-db('globals').push({
-  title: 'A new post',
-  shortname: 'newest-post',
-  description: 'A post about an even newer thing.',
-  date: 'May 26th, 2015',
-  abstract: 'It must be 3:23 PM somewhere, maybe space?'
+var db = low('db.json');
+var id = db('globals').size();
+
+prompt.start();
+prompt.get(['title', 'shortname', 'description', 'abstract'], function (err, result) {
+  if (!err) {
+    var template =
+"include ../../../includes/_head.jade\n\n\
+body\n\
+  #holder\n\
+    #content.container\n\
+      include ../../../includes/_header.jade\n\
+      .row\n\
+        section#welcome.eight.columns\n\
+          h3 " + result.title + "\n\
+          h4 " + result.description + "\n\
+          p\n\
+            em " + result.abstract + "\n\
+          //- Body goes here\n\
+    include ../../../includes/_footer.jade";
+
+    db('globals').push({
+      id: id++,
+      title: result.title,
+      shortname: result.shortname,
+      date: new Date(),
+      description: result.description,
+      abstract: result.abstract
+    });
+
+    fs.mkdirSync('./src/jade/main/articles/' + result.shortname);
+
+    fs.createWriteStream('./src/jade/main/articles/' + result.shortname + '/index.jade').write(template);
+
+  } else {
+    console.error('Error! ' + err);
+  }
 });
