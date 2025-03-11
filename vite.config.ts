293d98df0a163622a03cd604c995d548437635b8
Tim Douglas <neurotech@gmail.com>
2025-03-11 17:02:06 +1100

    Two spaces indentation

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,16 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "index.html"),
-				cv: resolve(__dirname, "cv/index.html"),
-				contact: resolve(__dirname, "contact/index.html"),
-			},
-		},
-	},
-	server: {
-		host: true,
-	},
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        cv: resolve(__dirname, "cv/index.html"),
+        contact: resolve(__dirname, "contact/index.html"),
+      },
+    },
+  },
+  server: {
+    host: true,
+  },
 });
