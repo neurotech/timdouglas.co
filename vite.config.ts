9e564918dc03d0dfb0d25ea02648055c7f8f5400
Tim Douglas <neurotech@gmail.com>
2025-03-11 15:28:07 +1100

    Refactored React out of the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
-import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				cv: resolve(__dirname, "cv/index.html"),
+				contact: resolve(__dirname, "contact/index.html"),
+			},
+		},
+	},
 	server: {
 		host: true,
 	},
-	plugins: [react()],
 });
